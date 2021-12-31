4a904e38ddeff916a958a51928d1c104551bddfa
Sinan Chaush <sinansonmez@outlook.com>
2021-12-31 12:23:06 +0100

    error related to security https is fixed

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import {ChakraProvider, ColorModeProvider} from '@chakra-ui/react'
 import theme from "../theme"
-import {createClient, Provider, useMutation} from "urql";
+import {createClient, Provider} from "urql";
 
-const client = createClient({url: "https://localhost:4000/graphql", fetchOptions: {credentials: "include"}});
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
 
-function MyApp({Component, pageProps}) {
+function MyApp({Component, pageProps}: any) {
   return (
     <Provider value={client}>
       <ChakraProvider resetCSS theme={theme}>
