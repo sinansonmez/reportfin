0311d4703362dff22ace5dcc3f2be63e5a63968f
Sinan Chaush <sinansonmez@outlook.com>
2021-12-31 12:57:55 +0100

    Register Error Handling is done

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,20 +4,28 @@ import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
 import {Box, Button} from "@chakra-ui/react";
 import {useRegisterMutation} from "../generated/graphql";
+import {toErrorMap} from "../utils/toErrorMap";
+import {useRouter} from "next/router";
 
 interface OwnProps {
 }
 
 type Props = OwnProps;
 
-const Register: FunctionComponent<Props> = (props) => {
+const Register: FunctionComponent<Props> = (_props) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{username: "", password: ""}}
-        onSubmit={async values => {
+        onSubmit={async (values, {setErrors}) => {
           const response = await register(values)
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors))
+          } else if (response.data?.register.user) {
+            await router.push("/")
+          }
         }}>
         {({isSubmitting}) => (
           <Form>
