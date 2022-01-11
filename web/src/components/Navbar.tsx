997b2ac4a2edb297fe9b7b65da4c9d33cabf869b
Sinan Chaush <sinansonmez@outlook.com>
2022-01-11 14:45:45 +0100

    report display styling is completed and title is added to navbar

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent} from 'react';
-import {Badge, Box, Button, Flex} from "@chakra-ui/react";
+import {Badge, Box, Button, Flex, Heading} from "@chakra-ui/react";
 import Nextlink from 'next/link';
 import {useLogoutMutation, useMeQuery} from "../generated/graphql";
 
@@ -18,37 +18,41 @@ const Navbar: FunctionComponent<Props> = (_props) => {
     // user not logged in
   } else if (!data?.me) {
     body = (
-      <>
-        <Nextlink href="/login">
-          <Button type="button" mr={2} colorScheme="blue">Login</Button>
-        </Nextlink>
-        <Nextlink href="/register">
-          <Button type="button" mr={2} colorScheme="blue">Register</Button>
-        </Nextlink>
-      </>
+      <Flex width="100%" justifyContent="space-between">
+        <Heading color="white">ReportFin</Heading>
+        <Box>
+          <Nextlink href="/login">
+            <Button type="button" mr={2} colorScheme="blue">Login</Button>
+          </Nextlink>
+          <Nextlink href="/register">
+            <Button type="button" mr={2} colorScheme="blue">Register</Button>
+          </Nextlink>
+        </Box>
+      </Flex>
     )
     // user logged in
   } else {
     body = (
-      <Flex>
-        <Badge mr={2} colorScheme="green" display="flex" alignItems="center">{data.me.username}</Badge>
-        <Nextlink href="/">
-          <Button
-            type="button"
-            mr={2}
-            colorScheme="blue"
-            onClick={() => logout()}
-            isLoading={logoutFetching}>Logout</Button>
-        </Nextlink>
+      <Flex width="100%" justifyContent="space-between">
+        <Heading color="white">ReportFin</Heading>
+        <Flex>
+          <Badge mr={2} colorScheme="green" display="flex" alignItems="center">{data.me.username}</Badge>
+          <Nextlink href="/">
+            <Button
+              type="button"
+              mr={2}
+              colorScheme="blue"
+              onClick={() => logout()}
+              isLoading={logoutFetching}>Logout</Button>
+          </Nextlink>
+        </Flex>
       </Flex>
     )
   }
 
   return (
-    <Flex zIndex={1} position="sticky" top={0} bgColor="blue.900" p={4} ml="auto">
-      <Box ml="auto">
-        {body}
-      </Box>
+    <Flex justifyContent="space-between" zIndex={1} position="sticky" top={0} bgColor="blue.900" p={4} ml="auto">
+      {body}
     </Flex>
   );
 };
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,35 +1,64 @@
-import {Button, Flex, Text} from "@chakra-ui/react";
+import {AlertIcon, Alert, Button, Flex, LinkOverlay, Stack, Text} from "@chakra-ui/react";
 import {withUrqlClient} from "next-urql";
 import {createUrqlClient} from "../utils/createUrqlClient";
 import Layout from "../components/Layout";
 import {useReportsQuery} from "../generated/graphql";
 import Nextlink from "next/link";
+import React from "react";
 
 //TODO: create and admin page with create bank, edit bank, delete bank, create report, edit report, delete report
 // TODO: why when I logged in, it doesn't show that I'm logged in?
 const Index = () => {
-  const [{data}] = useReportsQuery({
+  const [{data, fetching}] = useReportsQuery({
       variables: {limit: 10}
     }
   );
+
+  if (!fetching && !data) {
+    return <Alert mt={2} status='error'><AlertIcon/>Oops, something went wrong. Send me an email: sinansonmez@outlook.com. Please try again later</Alert>
+  }
+
+
+  const reportsView = (data: any[]) => {
+    return data.map(report =>
+      <Flex
+        px={5}
+        py={2}
+        shadow="md"
+        alignItems="center"
+        justifyContent="space-evenly"
+        borderWidth="1px"
+        borderRadius="4px"
+        key={report.id}>
+        <Text flexGrow="2">{report.bankId}</Text>
+        <Flex flexGrow="1" ml={2} alignItems="center">
+          <Text>{report.quarter}</Text>
+          <Text>-</Text>
+          <Text>{report.year}</Text>
+        </Flex>
+        <Button ml={2} colorScheme="blue" variant="outline">
+          <LinkOverlay href={"//" + report.link} isExternal>Download</LinkOverlay>
+        </Button>
+      </Flex>
+    )
+  }
   return (
     <Layout>
       <Nextlink href="/create-report">
         <Button colorScheme="blue">Create Report</Button>
       </Nextlink>
-      {!data?.reports ? (
+      {!data?.reports && fetching ? (
         <div>Loading...</div>
       ) : (
-        data.reports.map(report =>
-          <Flex key={report.id}>
-            <Text>{report.bankId}</Text>
-            <Text ml={2}>{report.link}</Text>
-            <Text ml={2}>{report.quarter}</Text>
-            <Text ml={2}>-</Text>
-            <Text ml={2}>{report.year}</Text>
-          </Flex>
-        ))
+        <Stack mt={2} spacing={2}>
+          {reportsView(data!.reports)}
+        </Stack>
+      )
       }
+      {data &&
+      <Flex p={8} justifyContent="center">
+        <Button isLoading={fetching} colorScheme="blue">Load More</Button>
+      </Flex>}
     </Layout>
   )
 }
