fae89c7f766f37d1e77e5715764a7f929469b098
Sinan Chaush <sinansonmez@outlook.com>
2022-01-21 00:03:27 +0100

    issue related to user logged out when refreshed is fixed

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -11,6 +11,7 @@ import {
 import {betterUpdateQuery} from "./betterUpdateQuery";
 import {pipe, tap} from "wonka";
 import Router from "next/router";
+import {isServer} from "./isServer";
 
 const errorExchange: Exchange = ({forward}) => (ops$) => {
   return pipe(
@@ -52,70 +53,77 @@ const cursorPagination = (): Resolver => {
   }
 }
 
-export const createUrqlClient = (ssrExchange: any) => ({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: "include" as const,
-  },
-  // cacheExchange is used to cache when user is logged in or registered or logged out
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      keys: {
-        PaginatedReports: () => null,
-      },
-      resolvers: {
-        Query: {
-          reports: cursorPagination(),
+export const createUrqlClient = (ssrExchange: any, ctx: any) => {
+  let cookie = "";
+  if (isServer()) {
+    cookie = ctx?.req?.headers?.cookie;
+  }
+  return {
+    url: "http://localhost:4000/graphql",
+    fetchOptions: {
+      credentials: "include" as const,
+      headers: cookie ? {cookie} : undefined,
+    },
+    // cacheExchange is used to cache when user is logged in or registered or logged out
+    exchanges: [
+      dedupExchange,
+      cacheExchange({
+        keys: {
+          PaginatedReports: () => null,
+        },
+        resolvers: {
+          Query: {
+            reports: cursorPagination(),
+          }
+        },
+        updates: {
+          Mutation: {
+            deleteReport: (_result, args, cache, info) => {
+              cache.invalidate({
+                __typename: "Report",
+                id: (args as DeleteReportMutationVariables).id,
+              });
+            },
+            // this is used to update cache (refetch new reports)  when user creates a new report
+            createReport: (_result, args, cache, info) => {
+              const allFields = cache.inspectFields("Query");
+              const fieldInfos = allFields.filter((info) => info.fieldName === "reports");
+              fieldInfos.forEach((fi) => {
+                cache.invalidate('Query', "reports", fi.arguments || {})
+              })
+            },
+            login: (_result, args, cache, _info) => {
+              betterUpdateQuery<LoginMutation, MeQuery>(
+                cache,
+                {query: MeDocument},
+                _result,
+                (result, query) => {
+                  return result.login.errors ? query : {me: result.login.user}
+                })
+            },
+            register: (_result, args, cache, _info) => {
+              betterUpdateQuery<RegisterMutation, MeQuery>(
+                cache,
+                {query: MeDocument},
+                _result,
+                (result, query) => {
+                  return result.register.errors ? query : {me: result.register.user}
+                })
+            },
+            logout: (_result, args, cache, _info) => {
+              betterUpdateQuery<LogoutMutation, MeQuery>(
+                cache,
+                {query: MeDocument},
+                _result,
+                (_result, _query) => {
+                  return {me: null}
+                })
+            },
+          }
         }
-      },
-      updates: {
-        Mutation: {
-          deleteReport: (_result, args, cache, info) => {
-            cache.invalidate({
-              __typename: "Report",
-              id: (args as DeleteReportMutationVariables).id,
-            });
-          },
-          // this is used to update cache (refetch new reports)  when user creates a new report
-          createReport: (_result, args, cache, info) => {
-            const allFields = cache.inspectFields("Query");
-            const fieldInfos = allFields.filter((info) => info.fieldName === "reports");
-            fieldInfos.forEach((fi) => {
-              cache.invalidate('Query', "reports", fi.arguments || {})
-            })
-          },
-          login: (_result, args, cache, _info) => {
-            betterUpdateQuery<LoginMutation, MeQuery>(
-              cache,
-              {query: MeDocument},
-              _result,
-              (result, query) => {
-                return result.login.errors ? query : {me: result.login.user}
-              })
-          },
-          register: (_result, args, cache, _info) => {
-            betterUpdateQuery<RegisterMutation, MeQuery>(
-              cache,
-              {query: MeDocument},
-              _result,
-              (result, query) => {
-                return result.register.errors ? query : {me: result.register.user}
-              })
-          },
-          logout: (_result, args, cache, _info) => {
-            betterUpdateQuery<LogoutMutation, MeQuery>(
-              cache,
-              {query: MeDocument},
-              _result,
-              (_result, _query) => {
-                return {me: null}
-              })
-          },
-        }
-      }
-    }),
-    errorExchange,
-    ssrExchange,
-    fetchExchange]
-})
+      }),
+      errorExchange,
+      ssrExchange,
+      fetchExchange]
+  }
+}
