2dce3bb0b1b169961f500525e037295c9f67f66d
Sinan Chaush <sinansonmez@outlook.com>
2022-01-22 15:26:06 +0100

    ssl changed to false

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const main = async () => {
     logging: true,
     migrations: [path.join(__dirname, "./migrations/*")],
     entities: [Bank, User, Report],
-    extra: {
+    ssl: {
       rejectUnauthorized: false,
     }
   })
