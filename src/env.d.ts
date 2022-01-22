aa4c0bded6b40ec4098a7aa0730b6195bdb9d961
Sinan Chaush <sinansonmez@outlook.com>
2022-01-22 09:19:55 +0100

    env updated for db passwords

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,6 +6,11 @@ declare global {
       PORT: string;
       SESSION_SECRET: string;
       CORS_ORIGIN: string;
+      PG_USER: string;
+      PG_PASSWORD: string;
+      PG_HOST: string;
+      PG_PORT: string;
+      PG_DATABASE: string;
     }
   }
 }
